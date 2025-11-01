f9b57ccd3c57c578689fdbf3bcb9355839920512
simdeistud <simone.deiana@studenti.units.it>
2025-11-01 13:31:09 +0100

    Initial Homework 1 commit

---
+++ HOMEWORK_01/include/Matrix.h
@@ -0,0 +1,32 @@
+#ifndef ADVANCED_PROGRAMMING_MATRIX_H
+#define ADVANCED_PROGRAMMING_MATRIX_H
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+class SparseMatrix
+{
+public:
+
+protected:
+
+};
+
+class SparseMatrixCOO final : public SparseMatrix
+{
+public:
+
+private:
+
+};
+
+class SparseMatrixCSR final : public SparseMatrix
+{
+public:
+
+
+private:
+
+};
+
+#endif //ADVANCED_PROGRAMMING_MATRIX_H
