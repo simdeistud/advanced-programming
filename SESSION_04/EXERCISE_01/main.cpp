bb91cca80357e934801f77c49f6b93f1142b06ed
simdeistud <simone.deiana@studenti.units.it>
2025-11-15 15:07:04 +0100

    Added inheritance tests and completed part 2/5 of exercise 1

--- SESSION_04/EXERCISE_01/main.cpp
+++ SESSION_04/EXERCISE_01/main.cpp
@@ -1,3 +1,19 @@
 //
 // Created by simone on 25/10/25.
 //
+
+#include "DataSource.h"
+
+int main()
+{
+    DataSource* fileTest = new FileDataSource("data.txt");
+    fileTest->display_info();
+    fileTest->read_data();
+    fileTest->display_info();
+
+    DataSource* consoleTest = new ConsoleDataSource();
+    consoleTest->display_info();
+    consoleTest->read_data();
+    consoleTest->display_info();
+    return 0;
+}
