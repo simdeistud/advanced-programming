d6cfd0442206eb3bb4932a311cd1217ca6ace39d
simdeistud <simone.deiana@studenti.units.it>
2025-11-15 15:44:03 +0100

    Created tests for data transformers and completed part 3/6 of exercise 1

--- SESSION_04/EXERCISE_01/main.cpp
+++ SESSION_04/EXERCISE_01/main.cpp
@@ -3,17 +3,47 @@
 //
 
 #include "DataSource.h"
+#include "DataTransformer.h"
 
 int main()
 {
     DataSource* fileTest = new FileDataSource("data.txt");
-    fileTest->display_info();
     fileTest->read_data();
+    std::cout << "Original data :" << std::endl;
     fileTest->display_info();
+    delete fileTest;
 
-    DataSource* consoleTest = new ConsoleDataSource();
-    consoleTest->display_info();
-    consoleTest->read_data();
-    consoleTest->display_info();
+    //DataSource* consoleTest = new ConsoleDataSource();
+    //consoleTest->display_info();
+    //consoleTest->read_data();
+    //consoleTest->display_info();
+
+    fileTest = new FileDataSource("data.txt");
+    fileTest->read_data();
+    DataTransformer* dataTransformer = new LinearScaler(fileTest, 10.0);
+    dataTransformer->transform();
+    delete dataTransformer;
+    std::cout << "Linear transformer with f=10 :" << std::endl;
+    fileTest->display_info();
+    delete fileTest;
+
+    fileTest = new FileDataSource("data.txt");
+    fileTest->read_data();
+    dataTransformer = new LogTransformer(fileTest);
+    dataTransformer->transform();
+    delete dataTransformer;
+    std::cout << "Log10 transformer :" << std::endl;
+    fileTest->display_info();
+    delete fileTest;
+
+    fileTest = new FileDataSource("data.txt");
+    fileTest->read_data();
+    dataTransformer = new StandardScaler(fileTest);
+    dataTransformer->transform();
+    delete dataTransformer;
+    std::cout << "Standard scaling between [0,1] :" << std::endl;
+    fileTest->display_info();
+    delete fileTest;
+
     return 0;
 }
