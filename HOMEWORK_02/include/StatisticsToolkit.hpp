3f502df18bdfe9fdcc02b22c18116d23d9790aad
simdeistud <simone.deiana@studenti.units.it>
2025-12-19 14:14:47 +0100

    Refactoring

--- HOMEWORK_02/include/StatisticsToolkit.hpp
+++ HOMEWORK_02/include/StatisticsToolkit.hpp
@@ -1,9 +1,13 @@
+#pragma once
+#include <vector>
+#include <unordered_map>
+
 class StatisticsToolkit{
 public:
-    static double mean(std::vector<double>& data);
-    static double median(std::vector<double>& data);
-    static double std_dev(std::vector<double>& data);
-    static double variance(std::vector<double>& data);
-    static std::size_t count(std::vector<double>& data, double& element);
-    static std::unordered_map frequency_map(std::vector<double>& data);
+    static double mean(const std::vector<double>& data);
+    static double median(const std::vector<double>& data);
+    static double std_dev(const std::vector<double>& data);
+    static double variance(const std::vector<double>& data);
+    static std::size_t count(const std::vector<double>& data, const double& element);
+    static std::unordered_map<double, std::size_t> frequency_map(const std::vector<double>& data);
 };
--- HOMEWORK_02/src/StatisticsToolkit.cpp
+++ HOMEWORK_02/src/StatisticsToolkit.cpp
@@ -1,33 +1,34 @@
 #include <vector>
+#include <unordered_map>
 #include <cmath>
 #include <numeric>
 #include <functional>
-#include <concepts>
+#include <algorithm>
 #include "StatisticsToolkit.hpp"
 
 double StatisticsToolkit::mean(const std::vector<double>& data) {
     return std::accumulate(data.begin(), data.end(), 0.0) / data.size();
 }
 
-double StatisticsToolkit::median(const std::vector<double>& data) {
+/*double StatisticsToolkit::median(const std::vector<double>& data) {
 
-}
+}*/
 
 double StatisticsToolkit::variance(const std::vector<double>& data) {
     auto m = StatisticsToolkit::mean(data);
-    return std::sqrt(std::transform_reduce(data.begin(), data.end(), std::plus, [](auto x) (return std::pow(x - m, 2))) / data.size())
+    return std::transform_reduce(data.begin(), data.end(), 0.0, std::plus<double>{}, [&m](double x) {return std::pow(x - m, 2);}) / data.size();
 }
 
 double StatisticsToolkit::std_dev(const std::vector<double>& data) {
     return std::sqrt(StatisticsToolkit::variance(data));
 }
 
-std::size_t StatisticsToolkit::count(const std::vector<double>& data, double& element) {
+std::size_t StatisticsToolkit::count(const std::vector<double>& data, const double& element) {
     return std::count(data.begin(), data.end(), element);
 }
 
-std::unordered_map StatisticsToolkit::frequency_map(const std::vector<double>& data) {
-    std::unordered_map<double, std::size_t> freq_map = std::unordered_map();
+std::unordered_map<double, std::size_t> StatisticsToolkit::frequency_map(const std::vector<double>& data) {
+    std::unordered_map<double, std::size_t> freq_map = std::unordered_map<double, std::size_t>();
     for (auto e : data) {
         if (freq_map.contains(e)) {
             ++freq_map.at(e);
