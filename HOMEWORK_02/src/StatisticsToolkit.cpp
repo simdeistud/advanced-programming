80241a220dd7f6a2107486aae2cd3c6670cd28a4
simdeistud <simone.deiana@studenti.units.it>
2025-12-19 02:18:01 +0100

    Initial implementation of some basic utilities

--- HOMEWORK_02/src/StatisticsToolkit.cpp
+++ HOMEWORK_02/src/StatisticsToolkit.cpp
@@ -1,3 +1,44 @@
-//
-// Created by simone on 12/18/25.
-//
+#include <vector>
+#include <cmath>
+#include <numeric>
+#include <functional>
+#include <concepts>
+#include "StatisticsToolkit.hpp"
+
+double StatisticsToolkit::mean(const std::vector<double>& data) {
+    return std::accumulate(data.begin(), data.end(), 0.0) / data.size();
+}
+
+double StatisticsToolkit::median(const std::vector<double>& data) {
+
+}
+
+double StatisticsToolkit::variance(const std::vector<double>& data) {
+    auto m = StatisticsToolkit::mean(data);
+    return std::sqrt(std::transform_reduce(data.begin(), data.end(), std::plus, [](auto x) (return std::pow(x - m, 2))) / data.size())
+}
+
+double StatisticsToolkit::std_dev(const std::vector<double>& data) {
+    return std::sqrt(StatisticsToolkit::variance(data));
+}
+
+std::size_t StatisticsToolkit::count(const std::vector<double>& data, double& element) {
+    return std::count(data.begin(), data.end(), element);
+}
+
+std::unordered_map StatisticsToolkit::frequency_map(const std::vector<double>& data) {
+    std::unordered_map<double, std::size_t> freq_map = std::unordered_map();
+    for (auto e : data) {
+        if (freq_map.contains(e)) {
+            ++freq_map.at(e);
+        } else {
+            freq_map.insert({e, 0});
+        }
+    }
+    return freq_map;
+}
+
+/* TODO
+ * - When templatizing the library, use the equality_comparable concept for the count and frequency map, and the totally_ordered concept for the median
+ * - The median should order pointers to the elements based on their value, as to maintain const correctness and proper performance
+ */
